032004942e6300d4334f02135c10c8aed60d55d8
Aideen Fay <fayai@tcd.ie>
2019-02-12 20:56:58 +0000

    tweak: hide books with no titles or images

--- app/src/main/java/com/example/library_application/Repository/JsonFileBookRepository.java
+++ app/src/main/java/com/example/library_application/Repository/JsonFileBookRepository.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class JsonFileBookRepository extends BookRepository {
     public JsonFileBookRepository(AppCompatActivity activity, int resourceId) {
@@ -26,13 +27,17 @@ public class JsonFileBookRepository extends BookRepository {
         try {
             JSONArray bookArray = new JSONArray(IOUtil.toString(fileStream));
 
-            Book[] books = new Book[bookArray.length()];
+            ArrayList<Book> books = new ArrayList<>(bookArray.length());
 
             for(int i = 0; i < bookArray.length(); i++) {
-                books[i] = Book.fromJSONObject(bookArray.getJSONObject(i));
+                Book book = Book.fromJSONObject(bookArray.getJSONObject(i));
+                if (book.getId() != null && book.getTitle() != null && book.getImages().length > 0)
+                {
+                    books.add(book);
+                }
             }
 
-            return books;
+            return books.toArray(new Book[0]);
         } catch (JSONException e) {
             e.printStackTrace();
             return new Book[0];
